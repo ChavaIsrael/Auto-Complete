41896d6b1e2f36ad16a5593a8ff7db8959e7e735
shirashindler <shiraframe@gmail.com>
2021-07-14 12:13:10 +0300

    basic

---
+++ DB/archiveDB.py
@@ -0,0 +1,15 @@
+
+
+archiveDB = {
+    1:"fbhdjvk rbfkregbrjhg rfbrjkgrbgjr bri briugbrg",
+    2:"hgifkhfgiuhgiuer fghuir huirtuigh. 4 98 br98g "
+}
+
+
+wordsDB = {"u":[
+        {"uir":[(2,[13])]},
+        {"uer":[(2,[20,25])]}
+    ]
+}
+
+print(archiveDB[2][13:])
